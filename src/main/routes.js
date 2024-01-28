738fd47b56c98e468d3c6645c0bcc4c4ec9f469a
Harsimran Virk <harsimranvirk99@gmail.com>
2024-01-28 15:09:03 +0530

    added storing checksums

--- src/main/routes.js
+++ src/main/routes.js
@@ -1,15 +1,36 @@
 import { Router } from "express"
 import { createHash } from 'crypto'
 import * as fs from 'fs'
+import { app } from 'electron'
 
 
 const router = Router()
+const hashCacheFilepath = app.getPath('userData') + "checksums.cache"
+
+const readChecksums = () => {
+    return new Promise(resolve => {
+        fs.readFile(hashCacheFilepath, (err, data) => {
+            try {
+                resolve(JSON.parse(data))
+            } catch (e) {
+                resolve({})
+            }
+        })
+    })
+}
+
+const writeChecksums = (checksums) => {
+    return new Promise(resolve => {
+        fs.writeFile(hashCacheFilepath, JSON.stringify(checksums), () => resolve())
+    })
+}
+
 
 const getChecksum = (filepath) => {
     return new Promise(resolve => {
         const chunkSize = 4096;
         const md5hash = createHash("md5")
-        const fstream = fs.createReadStream(filepath, {'highWaterMark': chunkSize})
+        const fstream = fs.createReadStream(filepath, { 'highWaterMark': chunkSize })
         fstream.on("data", (chunk) => {
             md5hash.update(chunk)
         })
@@ -18,11 +39,15 @@ const getChecksum = (filepath) => {
             resolve(checksum)
         })
     })
-} 
+}
 
-const getLastModified = (filepath) => {
+const getFileMetadata = (filepath) => {
     return new Promise(resolve => {
-        fs.stat(filepath, (err, stat) => resolve(stat.mtime.toISOString()))
+        fs.stat(filepath, (err, stat) => resolve({
+            lastModified: stat.mtime.toISOString(),
+            fileSize: stat.size / (1024 * 1024)
+        }
+        ))
     })
 }
 
@@ -30,16 +55,52 @@ router.get('/', (req, res) => {
     res.json({ api: 'share-play-v1', version: 0.1 })
 })
 
-router.post("/add", async (req, res) => {
+router.get("/videos", async (req, res) => {
+    const checksums = await readChecksums()
+    res.json(checksums)
+})
+
+router.post("/videos", async (req, res) => {
     const { filepath } = req.body
 
-    Promise.all([getChecksum(filepath), getLastModified(filepath)])
-        .then(([checksum, lastModified]) => res.json({
-            filepath,
-            checksum,
-            lastModified
-        }))
+    const checksums = await readChecksums()
+
+    Promise.all([getChecksum(filepath), getFileMetadata(filepath)])
+        .then(([checksum, {lastModified, fileSize}]) => {
+            return new Promise(async (resolve) => {
+                const data = {
+                    checksum,
+                    filepath,
+                    lastModified,
+                    fileSize
+                }
+                checksums[checksum] = data
+                await writeChecksums(checksums)
+                resolve(data)
+            })
+        })
+        .then(data => res.json(data))
 })
+
+router.patch("/videos", async (req, res) => {
+    const { checksum } = req.body
+    const checksums = await readChecksums()
+    if (checksums.hasOwnProperty(checksum)) {
+        delete checksums[checksum]
+        await writeChecksums(checksums)
+        res.json({
+            checksum,
+            removed: true
+        })
+    }
+    res.json({
+        checksum,
+        removed: false
+    })
+    
+})
+
+
 
 
 export { router }
